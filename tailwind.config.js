a007a0560ffbd6ad825a0a9f1eb278eb4bcff15b
AndrÃes Saa <afsaa@utp.edu.co>
2021-12-05 21:24:37 -0500

    Update project config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,19 +3,21 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}', './ui/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: {
-        light: '#f7b680',
-        DEFAULT: '#ef6c00',
-        dark: '#783600',
-      },
-      secondary: {
-        light: '#80c2f7',
-        DEFAULT: '#0084ef',
-        dark: '#004278',
+    extend: {
+      colors: {
+        primary: {
+          light: '#f7b680',
+          DEFAULT: '#ef6c00',
+          dark: '#783600',
+        },
+        secondary: {
+          light: '#80c2f7',
+          DEFAULT: '#0084ef',
+          dark: '#004278',
+        },
       },
     },
+    
     fontFamily: {
       montserrat: ['Montserrat', 'sans-serif'],
       cabin: ['Cabin', 'serif'],
