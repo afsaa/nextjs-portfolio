577a9d6e442a54faf024f4623cffc202afc9bfcc
AndrÃes Saa <afsaa@utp.edu.co>
2022-03-03 22:07:12 -0500

    Add navbar and navitem components

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import Link from 'next/link';
 
 import NavItem from './partials/NavItem';
+import Icon from '@/ui/icon';
 
-const Navbar = ({ navLinks }: { navLinks?: Array<any> }) => {
+const Navbar = ({ navItems }: { navItems?: Array<any> }) => {
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  const handleToggleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
   return (
-    <nav className="p-5 flex items-center justify-between bg-secondary-dark">
+    <nav className="p-5 md:p-20 flex items-center justify-between bg-primary-light">
       <Link href="/">
-        <a className="font-montserrat font-semibold text-xl text-white">AFSAA</a>
+        <a className="font-montserrat font-semibold text-2xl text-black">AFSAA</a>
       </Link>
-      <div className="flex bg-primary">
-        {navLinks.map(({ id, title, pathname }) => (
+      <div className={`${toggleMenu && 'hidden'} absolute top-20 left-56 md:static md:flex transition-all`}>
+        {navItems.map(({ id, title, pathname }) => (
           <NavItem id={id} title={title} pathname={pathname} />
         ))}
+      </div>
+      <div className="md:hidden cursor-pointer" onClick={() => handleToggleMenu()}>
+        <Icon className="" icon={<GiHamburgerMenu />} fontSize="25px" color="black" />
       </div>
     </nav>
   );
--- src/components/Navbar/partials/NavItem.tsx
+++ src/components/Navbar/partials/NavItem.tsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import Link from 'next/link'
+import React from 'react';
+import Link from 'next/link';
 
-const NavItem = ({ id, pathname, query='', title }: {id: number, pathname: string, query?: string, title: string}) => {
-    return (
-        <li key={id} className="mx-2 list-none hover:underline font-medium text-lg text-white">
-            <Link href={{ pathname, query }}>{title}</Link>
-        </li>
-    )
-}
+const NavItem = ({ id, pathname, query = '', title }: { id: number; pathname: string; query?: string; title: string }) => {
+  return (
+    <li key={id} className="mx-2 p-2 list-none no-underline rounded-md font-medium text-lg text-black hover:bg-secondary-light hover:drop-shadow-lg">
+      <Link href={{ pathname, query }}>{title}</Link>
+    </li>
+  );
+};
 
 export default NavItem;
