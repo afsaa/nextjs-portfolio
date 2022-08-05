9c73e6129fcf6bec8fc30df6dbc69bb06f4db3ff
Andres Saa <andressaa.dev@gmail.com>
2022-08-05 12:36:54 -0500

    Improve composition using render props

--- src/components/Hero/index.tsx
+++ src/components/Hero/index.tsx
@@ -1,5 +1,10 @@
-const Hero = ({ children }) => {
-  return <section className="m-0 md:my-4 px-10 py-5 md:py-10 flex flex-wrap items-center justify-around md:flex-nowrap">{children}</section>;
+const Hero = ({ error, onError, render }) => {
+  return (
+    <section className="m-0 md:my-4 px-10 py-5 md:py-10 flex flex-wrap items-center justify-around md:flex-nowrap">
+      {error && onError()}
+      {render && render()}
+    </section>
+  );
 };
 
 export default Hero;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,38 +1,30 @@
 import Head from 'next/head';
-import { useRouter } from 'next/router';
-import Image from 'next/image';
 import ContainerBlock from '@/components/ContainerBlock';
 import Hero from '@/components/Hero';
+import ErrorContainer from '../ui/error';
+import DescriptionWithCTA from '../ui/descriptionWithCTA';
+import ImageContainer from '../ui/imageContainer';
 import ExperiencePreview from '@/components/ExperiencePreview';
-import Button from '../ui/button';
 import useSWR from 'swr';
 
-const fetcher = (...args) => fetch(...args).then((res) => res.json());
+const fetcher = (url) => fetch(url).then((r) => r.json());
 
 export default function Home({ navItems }) {
-  const router = useRouter();
-  const { data: heroData } = useSWR(`api/contentful/entries/6mIZi967LZB1ytpguyNOR6`, fetcher);
-  const { data: experiencePreviewData } = useSWR(`api/contentful/entries/3JSTlKWt2HIcX4biS1eSqk`, fetcher);
+  const { data: heroData, error: heroError } = useSWR(`api/contentful/entries/6mIZi967LZB1ytpguyNOR6`, fetcher);
+  const { data: experiencePreviewData, error: experiencePreviewError } = useSWR(`api/contentful/entries/3JSTlKWt2HIcX4biS1eSqk`, fetcher);
 
   return (
     <ContainerBlock title="Andres Fernando Saa - Frontend Developer" navItems={navItems}>
-      <Hero>
-        <div className="w-full md:w-auto flex flex-col items-start">
-          <h1 className="mb-10 text-6xl text-mirage font-montserrat">{heroData?.fields?.name}</h1>
-          <h2 className="mb-5 text-3xl text-primary font-cabin font-bold">{heroData?.fields?.role}</h2>
-          <h4 className="mb-5 text-lg text-mirage font-montserrat">{heroData?.fields?.summary}</h4>
-          <Button text="See more" primary onClick={() => router.push('/about')} />
-        </div>
-        <div className="w-full md:w-auto mt-10 md:ml-2 flex items-center justify-center">
-          <Image
-            className="rounded-md drop-shadow-xl"
-            src={heroData?.fields?.profilePicture?.fields?.file?.url ? `https:${heroData?.fields?.profilePicture?.fields?.file?.url}` : '/me.jpg'}
-            alt="Photo of me"
-            width={500}
-            height={500}
-          />
-        </div>
-      </Hero>
+      <Hero
+        error={heroError}
+        onError={() => <ErrorContainer />}
+        render={() => (
+          <>
+            <DescriptionWithCTA fields={heroData?.fields} redirectUrl="/about" />
+            <ImageContainer fields={heroData?.fields} />
+          </>
+        )}
+      />
       <ExperiencePreview heading={experiencePreviewData?.fields?.experienceHeading} description={experiencePreviewData?.fields?.experienceDescription} />
     </ContainerBlock>
   );
