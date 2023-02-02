e033def6e19140661de9dd2fea510291796c3813
AndrÃes Saa <afsaa@utp.edu.co>
2023-02-01 21:24:43 -0500

    Add and config bundle analyzer

--- next.config.js
+++ next.config.js
@@ -1,5 +1,18 @@
-module.exports = {
+const withBundleAnalyzer = require('@next/bundle-analyzer')({
+  enabled: process.env.ANALIZE === 'true',
+});
+
+const config = {
+  future: {
+    webpack5: true,
+  },
   images: {
     domains: ['localhost', 'images.ctfassets.net'],
   },
+  i18n: {
+    locales: ['en-US', 'es-CO'],
+    defaultLocale: 'en-US',
+  },
 };
+
+module.exports = withBundleAnalyzer(config);
