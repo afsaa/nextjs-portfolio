a60741fcc3e3270708e37886b27b2d1621f4f17d
Andres Saa <andressaa.dev@gmail.com>
2023-02-25 10:38:46 -0500

    Handling exceptions for static data translation endpoint

--- src/pages/api/staticdata.ts
+++ src/pages/api/staticdata.ts
@@ -4,10 +4,16 @@ import { NextApiRequest, NextApiResponse } from 'next';
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   const { locale, componentName } = req.query;
-  //Find the absolute path of the json directory
-  const jsonDirectory = path.join(process.cwd(), `src/locales/${locale}`);
-  //Read the json data file data.json
-  const fileContents = await fs.readFile(jsonDirectory + `/${componentName}.json`, 'utf8');
-  //Return the content of the data file in json format
-  res.status(200).json(JSON.parse(fileContents));
+  if (!locale || !componentName) {
+    return res.status(400).end();
+  }
+  if (req.method === 'GET') {
+    //Find the absolute path of the json directory
+    const jsonDirectory = path.join(process.cwd(), `src/locales/${locale}`);
+    //Read the json data file data.json
+    const fileContents = await fs.readFile(jsonDirectory + `/${componentName}.json`, 'utf8');
+    //Return the content of the data file in json format
+    return res.status(200).json(JSON.parse(fileContents));
+  }
+  return res.status(405).end();
 }
