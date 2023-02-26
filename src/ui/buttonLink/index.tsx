db6d4656f36461c126342deeaa96d396adee6e0a
Andres Saa <andressaa.dev@gmail.com>
2023-02-26 14:52:31 -0500

    Update button link styling

--- src/ui/buttonLink/index.tsx
+++ src/ui/buttonLink/index.tsx
@@ -8,7 +8,7 @@ interface IButtonLink extends IButton {
 const ButtonLink = ({ type, text, active, onClick }: IButtonLink) => {
   return (
     <button className="bg-transparent" type={type} onClick={onClick}>
-      <p className={`${active ? 'font-normal' : 'font-bold'} ${active ? 'no-underline' : 'underline decoration-2 underline-offset-4'} font-cabin text-white`}>{text.toLocaleUpperCase()}</p>
+      <p className={`p-2 ${active ? 'font-normal' : 'font-bold'} ${active ? 'outline-none' : 'ring-2 ring-carrara'} font-cabin text-white`}>{text.toLocaleUpperCase()}</p>
     </button>
   );
 };
