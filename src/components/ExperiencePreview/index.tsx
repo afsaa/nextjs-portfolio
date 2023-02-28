550d25d56d7a34c710fbe09d1c3455e75b63c155
Andres Saa <andressaa.dev@gmail.com>
2023-02-27 23:03:59 -0500

    Fix spacing and add placeholder icon

--- src/components/ExperiencePreview/index.tsx
+++ src/components/ExperiencePreview/index.tsx
@@ -12,7 +12,7 @@ const ExperiencePreview = ({ heading, description }: IExperiencePreview) => {
   const router = useRouter();
 
   return (
-    <section className="m-0 md:mt-4 px-10 md:px-24 py-5 md:py-10 h-auto flex flex-col items-center justify-around">
+    <section className="m-0 h-auto flex flex-col items-center">
       <article className="w-full flex flex-col  items-center justify-between">
         <div className="flex items-center justify-center">
           <h2 className="text-3xl text-carrara font-montserrat font-bold">{heading}</h2>
