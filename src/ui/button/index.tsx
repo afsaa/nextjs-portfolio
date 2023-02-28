8a40126d1ab7bbd80259c81fbb7062303d68bd62
Andres Saa <andressaa.dev@gmail.com>
2023-02-27 23:03:09 -0500

    Update button component

--- src/ui/button/index.tsx
+++ src/ui/button/index.tsx
@@ -4,15 +4,18 @@ export interface IButton {
   type?: 'button' | 'submit' | 'reset';
   text: string;
   primary?: boolean;
+  size?: 'full';
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
 }
 
-const Button = ({ type, text, primary, onClick }: IButton) => {
+const Button = ({ type, text, primary, size, onClick }: IButton) => {
   return (
     <>
       <button
         type={type || 'button'}
-        className={`py-2 px-4 rounded-full font-cabin font-bold ${primary ? 'bg-primary-light' : 'bg-secondary-dark/80'} ${primary ? 'text-black' : 'text-carrara'} hover:opacity-80 focus:opacity-80`}
+        className={`${size === 'full' ? 'w-full' : ''} py-2 px-4 rounded-full font-cabin font-bold ${primary ? 'bg-primary-light' : 'bg-secondary-dark/80'} ${
+          primary ? 'text-black' : 'text-carrara'
+        } hover:opacity-80 focus:opacity-80`}
         onClick={onClick}
       >
         {text}
