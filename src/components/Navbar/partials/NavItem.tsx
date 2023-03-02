767304366473c6f039477ef8793f1f59fe50526d
Andres Saa <andressaa.dev@gmail.com>
2023-03-01 22:28:57 -0500

    Update page link styling

--- src/components/Navbar/partials/NavItem.tsx
+++ src/components/Navbar/partials/NavItem.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import Link from 'next/link';
 import { Sys } from '../../../generated/graphql';
+import { useRouter } from 'next/router';
 
 export type INavItem = { sys?: Sys; pathname?: string; query?: string; title?: string };
 
 const NavItem = ({ pathname, query = '', title }: INavItem) => {
+  const router = useRouter();
+
   return (
-    <li className="transition-all mx-2 p-2 list-none no-underline rounded-md font-montserrat font-medium text-lg text-carrara hover:bg-primary-light/50 hover:drop-shadow-lg">
+    <li
+      className={`transition-all mx-2 p-2 list-none no-underline font-montserrat font-medium text-lg text-carrara ${
+        pathname === router.route ? 'border-b-2 border-primary-light hover:drop-shadow-lg' : ''
+      } hover:border-b-2 border-primary-light hover:drop-shadow-lg`}
+    >
       <Link className="h-full" href={{ pathname, query }}>
         {title}
       </Link>
