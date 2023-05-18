5ffc676b5c03076ec1f49dd64f85483c48f5aee5
Andres Saa <andressaa.dev@gmail.com>
2023-05-17 22:18:25 -0500

    Update project card and icon components

--- src/components/ProjectCard/index.tsx
+++ src/components/ProjectCard/index.tsx
@@ -1,21 +1,28 @@
 import React from 'react';
 import { Project } from '../../generated/graphql';
+import Icon from '../../ui/icon';
 
 interface ProjectCardProps extends Project {}
 
-const ProjectCard = ({ title, description, technologies, image }: ProjectCardProps): JSX.Element => {
+const ProjectCard = ({ title, description, technologies, image, url }: ProjectCardProps): JSX.Element => {
   return (
     <div className="border border-stone-600 rounded-xl bg-black/50 shadow-md  overflow-hidden hover:opacity-80 focus:opacity-80">
       <img src={image?.url || 'https://via.placeholder.com/400x250'} alt={title} className="w-full h-48 object-fill" />
       <div className="p-4">
         <h3 className="mb-2 font-montserrat font-bold text-xl">{title}</h3>
-        <p className="mb-4 font-cabin text-carrara/50">{description}</p>
+        <p className="mb-4 font-cabin text-carrara/50 whitespace-normal">{description}</p>
         <div className="flex flex-wrap">
           {technologies.map((technology) => (
             <span key={technology} className="mr-2 mb-2 py-1 px-2 rounded-full bg-primary-light text-mirage  font-cabin font-medium text-sm">
               {technology}
             </span>
           ))}
+        </div>
+        <div className="mt-2">
+          <p className="flex items-center gap-2 text-white font-cabin">
+            <Icon icon="BiLink" fontSize="20px" color="white" />
+            {url}
+          </p>
         </div>
       </div>
     </div>
--- src/ui/icon/index.tsx
+++ src/ui/icon/index.tsx
@@ -2,21 +2,22 @@ import React from 'react';
 import { IconContext, IconType } from 'react-icons';
 import * as siIcons from 'react-icons/si';
 import * as giIcons from 'react-icons/gi';
+import * as biIcons from 'react-icons/bi';
 
-const allIcons = { ...siIcons, ...giIcons };
+const allIcons = { ...siIcons, ...giIcons, ...biIcons };
 
 type IconProps = {
-  className: string;
+  className?: string;
   icon: string;
   fontSize: string;
-  color: string;
+  color?: string;
 };
 
 const Icon = ({ className, icon, fontSize = '20px', color = 'black' }: IconProps): JSX.Element => {
   const ReactIcon: IconType = allIcons[icon];
 
   return (
-    <div className="flex items-center space-x-2">
+    <div className={`flex items-center space-x-2 ${className ? className : ''}`}>
       <IconContext.Provider value={{ style: { fontSize, color } }}>{ReactIcon && <ReactIcon />}</IconContext.Provider>
     </div>
   );
